b5a81505810269d180692808232201ee69572fdc
JohnAddy <dacooltype@gmail.com>
2020-10-07 13:41:40 +0300

    first push 2

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React, {useState} from 'react';
 import SageNavigation from './navigation/SageNavigation';
+import SignIn from "./screen/SignIn";
+import View from "react-native-web";
 
 export default function App(){
 
-    const [user, setUser] = useState([]);
+    const [user, setUser] = useState({token: ""});
 
     return(
         <SageNavigation />
-
-    { user &&
-
-    }
-    );
+        );
 
 
 }
