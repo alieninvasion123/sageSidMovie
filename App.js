623b52a50ee62d7bfcbbcd998a9d2049929e7157
JohnAddy <dacooltype@gmail.com>
2020-10-07 12:41:37 +0300

    first push 2

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SageNavigation from './navigation/SageNavigation';
 
 export default function App(){
+
+    const [user, setUser] = useState([]);
+
     return(
         <SageNavigation />
+
+    { user &&
+
+    }
     );
+
+
 }
--- screen/Welcome.js
+++ screen/Welcome.js
@@ -6,6 +6,7 @@ import {StyleSheet, Text, View, TouchableOpacity,Image,ImageBackground, Button} 
 
 
 export default class Welcome extends Component
+
 {
     render(){
 
